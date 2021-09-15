863ac6efe5e012d862fa094f2203b32b3d6df7ae
joizhang <zxlwhom@outlook.com>
2021-09-15 15:57:09 +0800

    feat: implementation of Java serialization.

---
+++ naive-rpc-netty/src/main/java/com/joizhang/naiverpc/netty/serialize/NativeJavaObjectOutput.java
@@ -0,0 +1,96 @@
+package com.joizhang.naiverpc.netty.serialize;
+
+import com.joizhang.naiverpc.serialize.ObjectOutput;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+
+public class NativeJavaObjectOutput implements ObjectOutput {
+
+    private final ObjectOutputStream outputStream;
+
+    public NativeJavaObjectOutput(OutputStream os) throws IOException {
+        this(new ObjectOutputStream(os));
+    }
+
+    protected NativeJavaObjectOutput(ObjectOutputStream out) {
+        Objects.requireNonNull(out);
+        this.outputStream = out;
+    }
+
+    protected ObjectOutputStream getObjectOutputStream() {
+        return outputStream;
+    }
+
+    @Override
+    public void writeBool(boolean v) throws IOException {
+        outputStream.writeBoolean(v);
+    }
+
+    @Override
+    public void writeByte(byte v) throws IOException {
+        outputStream.writeByte(v);
+    }
+
+    @Override
+    public void writeShort(short v) throws IOException {
+        outputStream.writeShort(v);
+    }
+
+    @Override
+    public void writeInt(int v) throws IOException {
+        outputStream.writeInt(v);
+    }
+
+    @Override
+    public void writeLong(long v) throws IOException {
+        outputStream.writeLong(v);
+    }
+
+    @Override
+    public void writeFloat(float v) throws IOException {
+        outputStream.writeFloat(v);
+    }
+
+    @Override
+    public void writeDouble(double v) throws IOException {
+        outputStream.writeDouble(v);
+    }
+
+    @Override
+    public void writeUTF(String v) throws IOException {
+        outputStream.writeUTF(v);
+    }
+
+    @Override
+    public void writeBytes(byte[] v) throws IOException {
+        if (v == null) {
+            outputStream.writeInt(-1);
+        } else {
+            writeBytes(v, 0, v.length);
+        }
+    }
+
+    @Override
+    public void writeBytes(byte[] v, int off, int len) throws IOException {
+        if (v == null) {
+            outputStream.writeInt(-1);
+        } else {
+            outputStream.writeInt(len);
+            outputStream.write(v, off, len);
+        }
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+        outputStream.flush();
+    }
+
+    @Override
+    public void writeObject(Object obj) throws IOException {
+        outputStream.writeObject(obj);
+    }
+
+}
---
+++ naive-rpc-netty/src/main/java/com/joizhang/naiverpc/netty/serialize/NativeJavaSerializer.java
@@ -0,0 +1,47 @@
+package com.joizhang.naiverpc.netty.serialize;
+
+import com.joizhang.naiverpc.serialize.ObjectInput;
+import com.joizhang.naiverpc.serialize.ObjectOutput;
+import com.joizhang.naiverpc.serialize.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.joizhang.naiverpc.serialize.Constants.NATIVE_JAVA_SERIALIZATION_ID;
+
+public class NativeJavaSerializer implements Serializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(NativeJavaSerializer.class);
+
+    private final static AtomicBoolean warn = new AtomicBoolean(false);
+
+    @Override
+    public byte getContentTypeId() {
+        return NATIVE_JAVA_SERIALIZATION_ID;
+    }
+
+    @Override
+    public String getContentType() {
+        return "x-application/nativejava";
+    }
+
+    @Override
+    public ObjectOutput serialize(OutputStream outputStream) throws IOException {
+        if (warn.compareAndSet(false, true)) {
+            logger.warn("Java serialization is unsafe.");
+        }
+        return new NativeJavaObjectOutput(outputStream);
+    }
+
+    @Override
+    public ObjectInput deserialize(InputStream inputStream) throws IOException {
+        if (warn.compareAndSet(false, true)) {
+            logger.warn("Java serialization is unsafe.");
+        }
+        return new NativeJavaObjectInput(inputStream);
+    }
+}
