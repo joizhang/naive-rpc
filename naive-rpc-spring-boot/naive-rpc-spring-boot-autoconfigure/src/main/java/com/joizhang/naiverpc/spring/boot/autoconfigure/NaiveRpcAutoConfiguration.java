2adf421cd0296e2efdeb00262af267843cf98dc3
joizhang <zxlwhom@outlook.com>
2023-02-22 22:39:09 +0800

    feat: update pom.xml of naive-rpc-spring-boot

--- naive-rpc-spring-boot/naive-rpc-spring-boot-autoconfigure/src/main/java/com/joizhang/naiverpc/spring/boot/autoconfigure/NaiveRpcAutoConfiguration.java
+++ naive-rpc-spring-boot/naive-rpc-spring-boot-autoconfigure/src/main/java/com/joizhang/naiverpc/spring/boot/autoconfigure/NaiveRpcAutoConfiguration.java
@@ -1,4 +1,14 @@
 package com.joizhang.naiverpc.spring.boot.autoconfigure;
 
+import com.joizhang.naiverpc.spring.boot.util.NaiveRpcUtils;
+import com.joizhang.naiverpc.spring.context.annotation.EnableNaiveRpcConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConditionalOnProperty(prefix = NaiveRpcUtils.NAIVE_RPC_PREFIX, name = "enabled", matchIfMissing = true)
+@Configuration
+@EnableConfigurationProperties(NaiveRpcConfigurationProperties.class)
+@EnableNaiveRpcConfig
 public class NaiveRpcAutoConfiguration {
 }
---
+++ naive-rpc-spring-boot/naive-rpc-spring-boot-autoconfigure/src/main/java/com/joizhang/naiverpc/spring/boot/util/NaiveRpcUtils.java
@@ -0,0 +1,10 @@
+package com.joizhang.naiverpc.spring.boot.util;
+
+public interface NaiveRpcUtils {
+
+    /**
+     * The prefix of property name of NaiveRpc
+     */
+    public static final String NAIVE_RPC_PREFIX = "naiverpc";
+
+}
