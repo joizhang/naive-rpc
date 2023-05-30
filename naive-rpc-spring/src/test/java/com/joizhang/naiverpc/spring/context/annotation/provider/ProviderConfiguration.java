1043fb15b0d3151a027a5ba47d50d14e69117b7c
joizhang <zxlwhom@outlook.com>
2023-05-30 14:36:20 +0800

    feat: update service provider

--- naive-rpc-spring/src/test/java/com/joizhang/naiverpc/spring/context/annotation/provider/ProviderConfiguration.java
+++ naive-rpc-spring/src/test/java/com/joizhang/naiverpc/spring/context/annotation/provider/ProviderConfiguration.java
@@ -4,6 +4,7 @@ import com.joizhang.naiverpc.spring.context.annotation.NaiveRpcComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @NaiveRpcComponentScan(basePackages = "com.joizhang.naiverpc.spring.context.annotation.provider")
+//@NaiveRpcComponentScan
 @PropertySource("classpath:/META-INF/default.properties")
 public class ProviderConfiguration {
 }
