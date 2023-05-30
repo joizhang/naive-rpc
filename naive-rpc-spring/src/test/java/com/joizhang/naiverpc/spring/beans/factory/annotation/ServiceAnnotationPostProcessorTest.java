1320dcf650b92d012b3c029b963e3558cb7065a2
joizhang <zxlwhom@outlook.com>
2023-05-30 14:38:08 +0800

    test: add ServiceAnnotationPostProcessorTest

--- naive-rpc-spring/src/test/java/com/joizhang/naiverpc/spring/beans/factory/annotation/ServiceAnnotationPostProcessorTest.java
+++ naive-rpc-spring/src/test/java/com/joizhang/naiverpc/spring/beans/factory/annotation/ServiceAnnotationPostProcessorTest.java
@@ -1,16 +1,22 @@
 package com.joizhang.naiverpc.spring.beans.factory.annotation;
 
+import com.joizhang.naiverpc.spring.context.annotation.EnableNaiveRpc;
 import com.joizhang.naiverpc.spring.context.annotation.NaiveRpcComponentScan;
-import org.junit.Assert;
-import org.junit.Test;
+import com.joizhang.naiverpc.spring.context.annotation.api.HelloService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
         ServiceAnnotationTestConfiguration.class,
         ServiceAnnotationPostProcessorTest.class,
@@ -18,8 +24,9 @@ import java.util.Map;
 })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @TestPropertySource(properties = {
-        "provider.package = org.apache.dubbo.config.spring.context.annotation.provider",
+        "provider.package = com.joizhang.naiverpc.spring.context.annotation.provider",
 })
+@EnableNaiveRpc(scanBasePackages = "${provider.package}")
 public class ServiceAnnotationPostProcessorTest {
 
     @Autowired
@@ -27,9 +34,12 @@ public class ServiceAnnotationPostProcessorTest {
 
     @Test
     public void testServiceAnnotationPostProcessor() {
+        Map<String, HelloService> helloServicesMap = beanFactory.getBeansOfType(HelloService.class);
+        assertEquals(1, helloServicesMap.size());
+
         Map<String, ServiceAnnotationPostProcessorTest> beanPostProcessorsMap =
                 beanFactory.getBeansOfType(ServiceAnnotationPostProcessorTest.class);
-        Assert.assertEquals(2, beanPostProcessorsMap.size());
+        assertEquals(1, beanPostProcessorsMap.size());
     }
 
     @NaiveRpcComponentScan({"org.apache.dubbo.config.spring.context.annotation", "${provider.package}"})
